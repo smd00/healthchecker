a87ed4632c7394143b43e7f0ada83ce3ab8c7d00
Daniel Montoya <daniel@montoya.com.au>
2020-09-03 13:57:38 +1000

    Changes to email script

--- send-email.py
+++ send-email.py
@@ -9,6 +9,25 @@ import os
 # from dotenv import load_dotenv
 # load_dotenv(dotenv_path='.env')
 
+SMDHC_MAIL_HOST = os.environ.get('SMTP_ADDRESS', '')
+SMDHC_MAIL_PORT = os.environ.get('SMTP_PORT', '')
+SMDHC_MAIL_USER = os.environ.get('SMTP_USERNAME', '')
+SMDHC_MAIL_PWD = os.environ.get('SMTP_PASSWORD', '')
+
+SMDHC_MAIL_SENDER = os.environ.get('SYSTEM_MAIL_FROM', '')
+SMDHC_MAIL_RECEIVER = os.environ.get('SYSTEM_MAIL_TO', '')
+
+# SMDHC_SOURCE_SEND_EMAIL_SCRIPT = os.environ.get('SMDHC_SOURCE', '') + '/send-email.py'
+
+# SMDHC_OUTPUT_FOLDER_PATH = '/home/root/app/log/smdhc'
+# SMDHC_OUTPUT_HEALTHCHECKS_FOLDER_PATH = os.environ.get('SMDHC_OUTPUT_FOLDER_PATH', SMDHC_OUTPUT_FOLDER_PATH) + '/healthchecks'
+# SMDHC_OUTPUT_ARCHIVE_FOLDER_PATH = os.environ.get('SMDHC_OUTPUT_FOLDER_PATH', SMDHC_OUTPUT_FOLDER_PATH) + '/archive'
+
+# SMDHC_CLIENT_LOG_FOLDER_PATH = '/home/root/app/log'
+# SMDHC_CLIENT_LOG_FILE_PATH = os.environ.get('SMDHC_CLIENT_LOG_FOLDER_PATH', SMDHC_CLIENT_LOG_FOLDER_PATH) + '/production.log'
+
+# SMDHC_CLIENT_NAME = 'Rails'
+
 '''
 Load args
 '''
@@ -29,24 +48,23 @@ logFileRead = logFile.read()
 '''
 Email content vars
 '''
-now = datetime.now()
-now_string = now.strftime('%d/%m/%Y %H:%M:%S')
-service = os.environ.get('SMDHC_CLIENT_NAME', '')
-subject = "Health Check: " + service
-
 import socket
 hostname = socket.gethostname()
 
+now = datetime.now()
+now_string = now.strftime('%d/%m/%Y %H:%M:%S')
+service = os.environ.get('SMDHC_CLIENT_NAME', hostname)
+subject = "Health Check: " + service
 '''
 Email vars
 '''
-port = os.environ.get('SMDHC_MAIL_PORT', '')
-smtp_server = os.environ.get('SMDHC_MAIL_HOST', '')
-login = os.environ.get('SMDHC_MAIL_USER', '')
-password = os.environ.get('SMDHC_MAIL_PWD', '')
+smtp_server = os.environ.get('SMDHC_MAIL_HOST', SMDHC_MAIL_HOST)
+port = os.environ.get('SMDHC_MAIL_PORT', SMDHC_MAIL_PORT)
+login = os.environ.get('SMDHC_MAIL_USER', SMDHC_MAIL_USER)
+password = os.environ.get('SMDHC_MAIL_PWD', SMDHC_MAIL_PWD)
 
-sender = os.environ.get('SMDHC_MAIL_SENDER', '')
-receiver = os.environ.get('SMDHC_MAIL_RECEIVER', '')
+sender = os.environ.get('SMDHC_MAIL_SENDER', SMDHC_MAIL_SENDER)
+receiver = os.environ.get('SMDHC_MAIL_RECEIVER', SMDHC_MAIL_RECEIVER)
 
 message = """\
 Subject: {}
@@ -62,7 +80,7 @@ Log: {}
 {}""".format(subject, receiver, sender, now_string, hostname, logFilePath, logFileRead)
 
 # print(message)
-print(smtp_server)
+print("smtp_server: " + smtp_server)
 
 '''
 Send email
